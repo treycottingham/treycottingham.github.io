536fb15fdac99a3c178e5214f9226c2d6f84f69f
treycottingham <trey.m.cottingham@gmail.com>
2017-09-23 15:35:55 -0600

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,338 @@
+var gameplay = [
+  {//0
+    "video":"",
+    "video2":"",
+    "link":"https://www.watchcartoononline.com/adventure-time-season-5-episode-12-vault-of-bones",
+    "image":"https://vignette4.wikia.nocookie.net/adventuretimewithfinnandjake/images/f/f3/S5_e12_Finn_and_Flame_Princess_running_into_a_dungeon.PNG/revision/latest?cb=20130219200825",  
+    "question":"Your name is Finn, you are the last living human in the world millenia after the end of human civilization.  Your girlfriend is the princess of the flame kingdom (she is made of fire, that will be important later).  She is feeling conflicted because everyone in her family is evil and she is not sure if she is good or evil herself.  To help her take her mind off things, you suggest finding and looting a dungeon, after all you are an adventurer and slaying evil things is your favorite thing to do.  You and Flame Princess set off in search of a dungeon armed only with your Demon Blood Sword and her magical fire-powers.",
+    "answers":[
+          {"title":"I'm ready, let's go find a dungeon!","response":1}
+          ]
+  },
+  {//1
+    "video":"",
+    "video2":"",
+    "link":"",
+    "image":"http://68.media.tumblr.com/tumblr_m1vcrzLAgO1r4ghkoo1_500.gif",  
+    "question":"You can't seem to find the entrances to any dungeons, lots of times there will be a switch hidden under a rock or something, so sometimes this takes a while.  Where do you look?",
+      "answers":[
+          {"title":"Under a rock","response":2},
+          {"title":"Knock on a tree, see if it's hollow","response":3},
+          {"title":"Ask FP (Flame Princess) if she has any ways of finding hidden things.","response":5},
+          ]
+  },
+  {//2 
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"http://68.media.tumblr.com/tumblr_m5sfqjXI7H1rn95k2o1_500.gif",
+      "question":"There's a bunch of beetles having a dance party under the rock, they squeek at you shrilly.  It seems like the rock is a nightclub and they can't see their laser effects in the daylight. You feel bad.",
+      "answers":[
+          {"title":"Apologize and put the rock back.","response":23},
+          {"title":"Tell them they're shaking it all wrong and show. them. how. it's. DONE.","response":4}
+          ]
+  },
+  {//3
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"http://68.media.tumblr.com/tumblr_ll473ajnYj1qixleeo1_500.gif",
+      "question":"You disturb a squirrel who gives you the evil eye.  Just ask your girlfriend for help already",
+      "answers":[
+          {"title":"Okay...","response":5},
+          {"title":"I will do this MY WAY! I'm checking under that rock!","response":2}
+          ]
+  },
+  {//4
+      "video":"",
+      "video2":"",
+      "link":"",  
+      "image":"https://i.giphy.com/media/13p77tfexyLtx6/giphy.webp",
+      "question":"Okay, I'll ask FP if she has any ideas.",
+      "answers":[
+          {"title":"Got any ideas, Flame P?","response":5}
+          ]
+  },
+  {//5
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"http://68.media.tumblr.com/250628773a7d020f023984e8743cbe48/tumblr_n17dn1xy831t5ct7wo3_r1_250.gif",
+      "question":"Flame Princess finds the entrance to a dungeon using her Heat Sense.",
+      "answers":[
+          {"title":"Wooooo, dungeon!!!","response":6}
+      ]
+  },
+  {//6
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"http://i.imgur.com/kdTS4n6.gif",
+      "question":"You enter the dungeon's skull-shaped entrance.  It's dark in here, do you need a torch?",
+      "answers":[
+          {"title":"Sure, torches are cool for dungeons.","response":7},
+          {"title":"Why would I need a torch? FP is literally made of fire.","response":7}
+          ]
+  },
+  {//7
+      "video":"https://www.youtube.com/watch?v=2oYJBtnPsp8&t=2s",
+      "video2":"",
+      "link":"",
+      "image":"https://i.ytimg.com/vi/_4bg_CeaZy0/hqdefault.jpg",
+      "question":"(Watch video for my absolute favorite Adventure time scene) You and FP come upon a one armed skeleton guard.  He sees you and approaches.  FP wants to burn him alive.",
+      "answers":[
+          {"title":"Let her burn him alive.","response":8},
+          {"title":"Ask the skeleton to excuse the two of you for a moment while you explain to her how to properly subdue an opponent through sheer confidence, then turn around, draw your sword and have a screaming match with the skeleton until he surrenders, then make him hand over all his loot and do the splits.","response":9}
+          ]
+    },
+    {//8
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"https://media0.giphy.com/media/4ArkzYLgtY316/giphy.gif",
+      "question":"Well I guess you're gonna miss out on some loot, at least she's having fun though. How do you proceed?",
+      "answers":[
+          {"title":"Sneak, that's how you get around in dungeons","response":9},
+          {"title":"Run","response":10}
+          ]
+    },
+    {//9
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"http://68.media.tumblr.com/0febe1ac2ae7d6182a2ee96d20706e85/tumblr_n4hledpowu1qgik10o1_500.gif",
+      "question":"How do you proceed",
+      "answers":[
+          {"title":"I'm pretty committed to sneaking, better just keep sneaking along...","response":9},
+          {"title":"I'm gonna run to the next interesting part.","response":10}
+          ]
+    },
+    {//10
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"https://vignette1.wikia.nocookie.net/adventuretimewithfinnandjake/images/c/cd/S5_e12_Skeleton_hologram.png/revision/latest?cb=20140310183611",
+      "question":"You come upon a large skeleton guard, too large to intimidate, what do you do?",
+      "answers":[
+          {"title":"Ask FP how good she is at quietly throwing a tiny bit of fire? When she throws a bit of fire to distract him, charge in and attack!","response":11},
+          {"title":"Charge in and attack!","response":11}
+          ]
+    },
+    {//11
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"https://vignette2.wikia.nocookie.net/adventuretimewithfinnandjake/images/b/b2/Dungeon1.jpg/revision/latest?cb=20110227005204",
+      "question":"It was just a hologram.",
+      "answers":[
+          {"title":"Move on.","response":13},
+          {"title":"Search the area for loot.","response":12}
+          ]
+    },
+    {//12
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"https://i.ytimg.com/vi/3SWEMQRPdAk/hqdefault.jpg",
+      "question":"You found a key",
+      "answers":[
+          {"title":"Move on so your girlfriend doesn't get bored.","response":13},
+          {"title":"Walk all the way back to the beginning of the dungeon where you think you remember seeing a chest that this key might unlock.","response":14}
+          ]
+    },
+    {//13
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"http://farm9.static.flickr.com/8161/7573478394_6ffc321b04_o.jpg",
+      "question":"You see a small hole in the wall behind where the hologram was.",
+      "answers":[
+          {"title":"Let FP take the lead.","response":15},
+          {"title":"Flirt a little first","response":20}
+          ]
+    },
+    {//14
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"https://vignette1.wikia.nocookie.net/adventuretimewithfinnandjake/images/1/18/DungeonCrawl.png/revision/latest?cb=20130226054753",
+      "question":"You found the chest, but the key didn't fit, oh well, time to move on.",
+      "answers":[
+          {"title":"Move on","response":13}
+          ]
+    },
+    {//15
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"https://media1.giphy.com/media/TKquL9qWr4nKM/giphy.gif",
+      "question":"FP goes a little nanners and starts burning everything in the dungeon",
+      "answers":[
+          {"title":"She clearly needs to vent, lets give her some room to let it all out.","response":16},
+          {"title":"Try to keep up as she runs through the burning dungeon.","response":16}
+          ]
+    },
+    {//16
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"https://media1.giphy.com/media/ZuYnTKDgb32rm/giphy.gif",
+      "question":"You are suddenly attacked by a swarm of combustible skeletons",
+      "answers":[
+          {"title":"Draw your sword and start slaying evil creatures!","response":17},
+          {"title":"Get grabbed by their leader and wrapped up in a gooey cocoon.","response":18}
+          ]
+    },
+    {//17
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"https://vignette4.wikia.nocookie.net/adventuretimewithfinnandjake/images/1/1d/S5e12_Goo4.png/revision/latest?cb=20131231223034",
+      "question":"Your attempt to resist fails and you are nabbed by their leader who turns you into a little teen boy burrito.",
+      "answers":[
+          {"title":"Wait for help from FP.","response":19},
+          {"title":"Yell to FP not to use fire, as that will cook you alive.","response":19}
+          ]
+    },
+    {//18
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"https://vignette4.wikia.nocookie.net/adventuretimewithfinnandjake/images/1/1d/S5e12_Goo4.png/revision/latest?cb=20131231223034",
+      "question":"Their leader who turns you into a little teen boy burrito.",
+      "answers":[
+          {"title":"Wait for help from FP.","response":19},
+          {"title":"Yell to FP not to use fire, as that will cook you alive.","response":19}
+          ]
+    },
+    {//19
+      "video":"",
+      "video2":"https://www.youtube.com/watch?v=gjASJwBm4eo",
+      "link":"",
+      "image":"",
+      "question":"(Watch video for the exciting climax of the story) Flame Princess saves your butt and you leave the dungeon with the chest from before.",
+      "answers":[
+          {"title":"Open the chest.","response":21},
+          {"title":"Put on your flameproof aluminum suit and hug your girlfriend.","response":22}
+          ]
+    },
+    {//20
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"https://media1.giphy.com/media/LwB6RLPZlxT0Y/giphy.gif",
+      "question":"You flirt.",
+      "answers":[
+          {"title":"Let FP take the lead.","response":15}
+          ]
+    },
+    {//21
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"https://i.ytimg.com/vi/XaDRTCDsb5Q/hqdefault.jpg",
+      "question":"Hooray...?",
+      "answers":[
+          {"title":"You win, start over?","response":0}
+          ]
+    },
+    {//22
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhbRcyXNGmv015l6gf1mSBoU8_2IEVUFCVOaPl5NpJPjLQGYVAlw",
+      "question":"Mathematical.",
+      "answers":[
+          {"title":"You win, start over?","response":0}
+          ]
+    },
+    {//23
+      "video":"",
+      "video2":"",
+      "link":"",
+      "image":"http://68.media.tumblr.com/tumblr_m1vcrzLAgO1r4ghkoo1_500.gif",  
+      "question":"You still can't find the entrance, where do you look?",
+        "answers":[
+            {"title":"Knock on a tree, see if it's hollow","response":3},
+            {"title":"Ask FP (Flame Princess) if she has any ways of finding hidden things.","response":5},
+            ]
+    },
+    ];
+
+var currentLocation = 0;
+//var inventory = [];
+
+window.printCurrentLocation = function(){
+  document.getElementById("question").innerHTML = gameplay[currentLocation].question;
+  //document.getElementById("image").src = gameplay[currentLocation].image;
+  document.getElementById("image").src = gameplay[currentLocation].image;
+  document.getElementById("link").innerHTML = gameplay[currentLocation].link;
+  document.getElementById("video").innerHTML = gameplay[currentLocation].video;
+  document.getElementById("video2").innerHTML = gameplay[currentLocation].video2;
+  //document.getElementById("image3").src = gameplay[currentLocation].image3;
+  var answers = "";
+  for(var i=0;i<gameplay[currentLocation].answers.length;i++){
+  //for(var i=0,l=gameplay[currentLocation].answers.length;i<l;i++){    original code
+      answers += "<p><button onclick='setLocation("+gameplay[currentLocation].answers[i].response+")'>"+gameplay[currentLocation].answers[i].title+"</button></p>";
+  }
+  document.getElementById("answers").innerHTML = answers;
+}
+
+window.setLocation = function(num) {
+  currentLocation = num;
+  window.printCurrentLocation();
+}
+
+window.printCurrentLocation();
+
+
+/*Create functions for options one and two
+
+let button1 = document.getElementById('button1');
+let button2 = document.getElementById('button2');
+let stages = document.getElementById('stages');
+let situations = document.getElementById('situations');
+
+var story = {
+  currentSituation: {},
+  currentOption1: {},
+  currentOption2: {},
+
+}
+
+//<iframe width="640" height="360" src="https://www.youtube.com/embed/2oYJBtnPsp8" frameborder="0" allowfullscreen></iframe>
+//https://www.youtube.com/embed/2oYJBtnPsp8
+
+button1.addEventListener('click', option1);
+button2.addEventListener('click', option2);
+
+//function replace() {
+//  situations = ''
+//}
+
+function option1() {
+  situations.innerHTML = ''
+}
+
+function option2() {
+  alert ("You chose option 2");
+}
+
+function getCurrentSituation(currentSituation) {
+  var currentSituation = {};
+}
+
+
+//document.getElementById('situations').*/
+
+
+
+//pixel img of rock http://piskel-imgstore-b.appspot.com/img/b334771c-9e4d-11e7-8375-93de5053410d.gif
+//img of tree  http://piskel-imgstore-b.appspot.com/img/64a30d87-9e4e-11e7-a0f5-93de5053410d.gif
+//img of fp   http://piskel-imgstore-b.appspot.com/img/9dc5308f-9e4f-11e7-8502-93de5053410d.gif
+
+
+//light torch
+//pick up gold piece
+//come across large skeleton
+//
